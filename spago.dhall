f3a04d782f8e8dac0a97a5a70549ea4613418fd2
Jonathan Merritt <j.s.merritt@gmail.com>
2020-06-21 17:09:36 +1000

    Threejs cube rendering

--- spago.dhall
+++ spago.dhall
@@ -3,7 +3,13 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "my-project"
-, dependencies = [ "console", "effect", "psci-support", "purescript-three" ]
+, dependencies =
+  [ "console"
+  , "effect"
+  , "psci-support"
+  , "purescript-three"
+  , "web-html"
+  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Main.purs
+++ src/Main.purs
@@ -1,15 +1,66 @@
 module Main where
 
 import Prelude
+
+import Data.Maybe (Maybe(Just))
 import Effect (Effect)
 import Effect.Console (log)
+import Partial.Unsafe as Unsafe
 
-import Graphics.Three.Renderer
+import Graphics.Three.Camera (class Camera)
+import Graphics.Three.Camera as Camera
+import Graphics.Three.Geometry as Geometry
+import Graphics.Three.Material as Material
+import Graphics.Three.Object3D as Object3D
+import Graphics.Three.Renderer (Renderer)
+import Graphics.Three.Renderer as Renderer
+import Graphics.Three.Scene (Scene)
+import Graphics.Three.Scene as Scene
+
+import Web.DOM.NonElementParentNode as NonElementParentNode
+import Web.HTML as HTML
+import Web.HTML.HTMLDocument as HTMLDocument
+import Web.HTML.Window (Window)
+import Web.HTML.Window as Window
 
 main :: Effect Unit
-main = do
+main = void $ Unsafe.unsafePartial do
   log "Starting main"
 
-  renderer <- createWebGL {antialias: true}
+  window <- HTML.window
+  document <- Window.document window
+  Just canvasElement <-
+    NonElementParentNode.getElementById
+      "exteriorSphere"
+      (HTMLDocument.toNonElementParentNode document)
+
+  let aspect = 1.0
+
+  renderer <- Renderer.createWebGL {antialias: true, canvas: canvasElement}
+  Renderer.setSize renderer 400.0 400.0
+  scene <- Scene.create
+  camera <- Camera.createPerspective 75.0 aspect 0.1 1000.0
+
+  geometry <- Geometry.createBox 1.0 1.0 1.0
+  material <- Material.createMeshBasic {color: 0x00ff00}
+  cube <- Object3D.createMesh geometry material
+  Scene.addObject scene cube
+
+  Object3D.setPosition camera 0.0 0.0 2.0
+
+  render window renderer scene camera
 
   log "finished main"
+
+render ::
+  forall cam. Camera cam =>
+  Window ->
+  Renderer ->
+  Scene ->
+  cam ->
+  Effect Unit
+render window renderer scene camera = do
+  Renderer.render renderer scene camera
+  void $ Window.requestAnimationFrame
+    (render window renderer scene camera)
+    window
